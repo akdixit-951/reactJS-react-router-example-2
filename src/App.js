fd0956cda6270361d61b0c961bc0290ade7dc526
root <akdixit951@gmail.com>
2023-12-08 18:25:55 +0000

    react router example

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// ccbp start RJSIVADE1L
+
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
+import Header from './components/Header'
+import About from './components/About'
+import Contact from './components/Contact'
+import BlogsList from './components/BlogsList'
+import NotFound from './components/NotFound'
+
+import './App.css'
+
+const App = () => (
+  <BrowserRouter>
+    <Header />
+    <Switch>
+      <Route exact path="/" component={BlogsList} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
+)
+
+export default App
